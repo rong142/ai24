01bab522e39ccd848a926286b803bcaac439a591
rong142 <v073099@gmail.com>
2025-04-15 23:42:36 +0800

    全文關聯分析

--- website_configs/urls.py
+++ website_configs/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('topperson/', include('app_top_person.urls')),
     path('userkeyword/', include('app_user_keyword.urls')),
     path('shotime/', include('app_shotime.urls')),
+    path('userkeyword_assoc/', include('app_user_keyword_association.urls')),
 ]
